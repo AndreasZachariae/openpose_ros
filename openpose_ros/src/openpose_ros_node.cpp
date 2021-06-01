b9ddcecc131da9f23324f567105d241a636bd871
Andreas Zachariae <zaan0001@hs-karlsruhe.de>
2021-06-01 12:45:43 +0200

    Changes to config and flags

--- openpose_ros/src/openpose_ros_node.cpp
+++ openpose_ros/src/openpose_ros_node.cpp
@@ -3,16 +3,16 @@
 // when he desires to.
 
 // This example shows the user how to use the OpenPose wrapper class:
-    // 1. User reads images
-    // 2. Extract and render keypoint / heatmap / PAF of that image
-    // 3. Save the results on disk
-    // 4. User displays the rendered pose
-    // Everything in a multi-thread scenario
+// 1. User reads images
+// 2. Extract and render keypoint / heatmap / PAF of that image
+// 3. Save the results on disk
+// 4. User displays the rendered pose
+// Everything in a multi-thread scenario
 // In addition to the previous OpenPose modules, we also need to use:
-    // 1. `core` module:
-        // For the Array<float> class that the `pose` module needs
-        // For the Datum struct that the `thread` module sends between the queues
-    // 2. `utilities` module: for the error & logging functions, i.e. op::error & op::opLog respectively
+// 1. `core` module:
+// For the Array<float> class that the `pose` module needs
+// For the Datum struct that the `thread` module sends between the queues
+// 2. `utilities` module: for the error & logging functions, i.e. op::error & op::opLog respectively
 // This file should only be used for the user to take specific examples.
 
 // C++ std library dependencies
@@ -27,7 +27,7 @@ int openPoseROS()
 {
     // logging_level
     op::checkBool(0 <= FLAGS_logging_level && FLAGS_logging_level <= 255, "Wrong logging_level value.",
-              __LINE__, __FUNCTION__, __FILE__);
+                  __LINE__, __FUNCTION__, __FILE__);
     op::ConfigureLog::setPriorityThreshold((op::Priority)FLAGS_logging_level);
     op::Profiler::setDefaultX(FLAGS_profile_speed);
 
@@ -40,15 +40,12 @@ int openPoseROS()
     openPose.start();
 
     // OpenPose processing
-    auto options = rclcpp::NodeOptions().
-        allow_undeclared_parameters(true).
-        automatically_declare_parameters_from_overrides(true);
+    auto options = rclcpp::NodeOptions().allow_undeclared_parameters(true).automatically_declare_parameters_from_overrides(true);
     auto openPoseROSIO = std::make_shared<openpose_ros::OpenPoseROSIO>(options, openPose);
     auto sub = image_transport::create_subscription(openPoseROSIO->getPtr().get(),
-        openPoseROSIO->image_topic_.as_string(),
-        std::bind(&openpose_ros::OpenPoseROSIO::processImage, openPoseROSIO, std::placeholders::_1),
-        openPoseROSIO->input_image_transport_type_.as_string()
-    );
+                                                    openPoseROSIO->image_topic_.as_string(),
+                                                    std::bind(&openpose_ros::OpenPoseROSIO::processImage, openPoseROSIO, std::placeholders::_1),
+                                                    openPoseROSIO->input_image_transport_type_.as_string());
 
     rclcpp::spin(openPoseROSIO);
 
@@ -58,10 +55,8 @@ int openPoseROS()
 
     // Measuring total time
     const auto now = std::chrono::high_resolution_clock::now();
-    const auto totalTimeSec = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(now-timerBegin).count()
-                            * 1e-9;
-    const auto message = "Real-time pose estimation demo successfully finished. Total time: "
-                       + std::to_string(totalTimeSec) + " seconds.";
+    const auto totalTimeSec = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(now - timerBegin).count() * 1e-9;
+    const auto message = "Real-time pose estimation demo successfully finished. Total time: " + std::to_string(totalTimeSec) + " seconds.";
     op::opLog(message, op::Priority::High);
 
     rclcpp::shutdown();
